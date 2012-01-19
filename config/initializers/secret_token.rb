6551b44cbe15099631eb86b0bd365ccc709976f4
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2012-01-19 15:57:53 +0900

    create rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeaRoom::Application.config.secret_token = '22b6b9fdda988dc32bf8fb2ba96a97257c666cec5471be789560621b95d65d30c3ff5a8acc044a975d67206bbc7e2e699b5bdd8034019f4967d57bffa79d717f'
